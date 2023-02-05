8655c8bb5408d26f1af1c6113e6db1464b5af1a1
Gabriel Alves <111418442+Biel-Alves@users.noreply.github.com>
2023-02-05 14:05:41 +0100

    exercise done

--- src/be/intectbrussel/MainApp.java
+++ src/be/intectbrussel/MainApp.java
@@ -2,8 +2,10 @@ package be.intectbrussel;
 
 public class MainApp {
     public static void main(String[] args) {
-
+        System.out.println("Github");
         System.out.println("---- Oefening 1: Oppervlakte Berekening ----");
+        // 1. Schrijf een programma om de oppervlakte van een rechthoek te berekenen.
+        // De rechthoek is 12,5 cm lang en 7,8 cm breed.
         double l = 12.5;
         double b = 7.8;
 
@@ -11,6 +13,8 @@ public class MainApp {
         System.out.println("Oppervlakte: " + res + " cm²");
 
         System.out.println("---- oefening 2: Logical Operators ----");
+        // 2. Vergelijk deze 2 variabelen: bool01 = true en bool02 = false.
+        // Gebruik hiervoor alle 3 de logical operators voor.
         boolean bool01 = true;
         boolean bool02 = false;
 
@@ -24,13 +28,13 @@ public class MainApp {
         System.out.println("NOT vergelijking : " + result);
         // of
         result = !bool01;
-        System.out.println("NOT vergeleking bool01 : " + result);
+        System.out.println("NOT vergelijking bool01 : " + result);
 
         result = !bool02;
-        System.out.println("NOT vergeleking bool02 : " + result);
+        System.out.println("NOT vergelijking bool02 : " + result);
 
         System.out.println("--- Oefening 3: Relational Operators ----");
-
+        // Gebruik de Relational operators op de variabelen hieronder. Bekijk alle mogelijkheden om deze te gebruiken.
         byte bNum = 10;
         int iNum = 5;
         long lNum = 5;
@@ -68,7 +72,7 @@ public class MainApp {
         result = (iNum < lNum);
         System.out.println("(iNum < lNum) : " + result);
 
-        System.out.println("---- Mogelikheden met (>) ----");
+        System.out.println("---- Mogelijkheden met (>) ----");
 
         result = (bNum > iNum);
         System.out.println("(bNum > iNum) : " + result);
@@ -102,10 +106,14 @@ public class MainApp {
         System.out.println("(iNum >= lNum) : " + result);
 
         System.out.println("--- Oefening 4: Assignment Operators ----");
+        // 4. Maak gebruik van de Assignment Operators op de volgende variabelen. Ga na wat wel werkt en wat niet.
+        // Probeer ze ook te wisselen bijvoorbeeld:
+        // System.out.println(floatNum += shortNum);
+        // System.out.println(shortNum += floatNum);
 
         short shortNum = 101;
         float floatNum = 10.99F;
-        boolean booVar = true; // geen mogelijke manier gevonden om deze te gebruiken
+        boolean booVar = true; // geen mogelijke manier gevonden om deze te gebruiken.
 
         System.out.println("---- (+=) ----");
         System.out.println(shortNum += floatNum);
--- src/be/intectbrussel/Oefenreeks1.java
+++ src/be/intectbrussel/Oefenreeks1.java
@@ -2,7 +2,58 @@ package be.intectbrussel;
 
 public class Oefenreeks1 {
     public static void main(String[] args) {
+        System.out.println("Operators - Oefenreeks - 1");
+        System.out.println("---- Oefening - 1 ----");
+        // 1. Declareer twee integer variabelen, "a" en "b", en geef ze de waarden 5 en 10.
+        int a = 5;
+        int b = 10;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
+        System.out.println("---- Oefening - 2 ----");
+        // 2. Gebruik de plus operator (+) om de som van "a" en "b" te berekenen en print het resultaat.
+        int res = a + b;
+        System.out.println("Som resultaat = " + res);
+
+        System.out.println("---- Oefening - 3 ----");
+        // 3. Gebruik de min operator (-) om het verschil tussen "a" en "b" te berekenen en print het resultaat.
+        res = a - b;
+        System.out.println("Aftrekking resultaat = " + res);
+
+        System.out.println("---- Oefening - 4 ----");
+        // 4. Gebruik de maal operator (*) om het product van "a" en "b" te berekenen en print het resultaat.
+        res = a * b;
+        System.out.println("Vermenigvuldiging resultaat = " + res);
+
+        System.out.println("---- Oefening - 5 ----");
+        // 5. Gebruik de gedeeld door operator (/) om het quotient van "a" en "b" te berekenen en print het resultaat.
+        res = a / b;
+        System.out.println("Deling resultaat = " + res);
+
+        System.out.println("---- Oefening - 6 ----");
+        // 6. Gebruik de modulo operator (%) om de rest van "a" gedeeld door "b" te berekenen en print het resultaat.
+        res = a % b;
+        System.out.println("Rest resultaat = " + res);
+
+        System.out.println("---- Oefening - 7 ----");
+        // 7. Gebruik de increment operator (++) om de waarde van "a" te verhogen met 1 en print het resultaat.
+        int inc = ++a;
+        System.out.println("Verhoogde resultaat = " + inc);
+
+        System.out.println("---- Oefening - 8 ----");
+        // 8. Gebruik de decrement operator (--) om de waarde van "a" te verlagen met 1 en print het resultaat.
+        int dec = --a;
+        System.out.println("Verlaagde resultaat = " + dec);
+
+        System.out.println("---- Oefening - 9 ----");
+        // 9. Gebruik de gelijk aan vergelijkingsoperator (==) om te controleren of "a" gelijk is aan "b" en print het resultaat.
+        Boolean boolRes = a == b;
+        System.out.println("Vergelijkingsresultaat = " + boolRes);
+
+        System.out.println("---- Oefening - 10 ----");
+        // 10. Gebruik de niet gelijk aan vergelijkingsoperator (!=) om te controleren of "a" ongelijk is aan "b" en print het resultaat.
+        boolRes = a != b;
+        System.out.println("Vergelijkingsresultaat = " + boolRes);
     }
 
 }
